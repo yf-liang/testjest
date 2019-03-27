4987c943b7ba520f3398cb65e6874572837d3efc
Ye-Fee Liang <yf.liang@yahoo.ca>
2019-03-27 12:01:06 -0700

    Mar-27-2019 12:01;: new proj

---
+++ sketch.test.js
@@ -0,0 +1,11 @@
+const sum = require('./sketch');
+
+// test('add 1 + 2 to equal 3', () => {
+//   expect(sum(1,2)).toBe(3);
+// });
+
+test('add 1 + 2 to equal 3', sumTest);
+
+function sumTest() {
+  expect(sum(1,2)).toBe(3);
+}
